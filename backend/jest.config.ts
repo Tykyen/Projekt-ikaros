7accd1af237279a219742c38eb7deafd767ef9dd
PJ <tykytanjunior@gmail.com>
2026-05-05 14:34:34 +0200

    fix(parity-check): rename comparator.spec.ts -> test.ts, adjust jest.config testMatch

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -4,8 +4,7 @@ import type { Config } from 'jest';
 const config: Config = {
   moduleFileExtensions: ['js', 'json', 'ts'],
   rootDir: '.',
-  roots: ['<rootDir>/src', '<rootDir>/scripts'],
-  testRegex: '.*\\.spec\\.ts$',
+  testMatch: ['<rootDir>/src/**/*.(spec|test).ts', '<rootDir>/scripts/**/*.(spec|test).ts'],
   transform: { '^.+\\.(t|j)s$': 'ts-jest' },
   collectCoverageFrom: ['src/**/*.(t|j)s'],
   coverageDirectory: './coverage',
